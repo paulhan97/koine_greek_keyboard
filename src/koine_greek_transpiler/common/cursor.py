afb28e90ec4e217a55744c0c6e6db45d7bda7b81
Paul Han <paulh@dtric.com>
2025-03-04 09:58:40 -1000

    Add cursor action interface and add character implementation

--- src/koine_greek_transpiler/common/cursor.py
+++ src/koine_greek_transpiler/common/cursor.py
@@ -1,7 +1,18 @@
+from koine_greek_transpiler.common.cursorable import Cursorable
+
+import abc
+
+
+class CursorAction(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def run():
+        raise NotImplementedError
+
+
 class Cursor:
-    def __init__(self, initial_position: int = 0, initial_text: str = ''):
+    def __init__(self, cursorable: Cursorable, initial_position: int = 0):
+        self.cursorable = cursorable
         self.position = initial_position
-        self.text = initial_text
 
     def place_at(self, new_position: int):
         self.position = new_position
@@ -11,13 +22,25 @@ class Cursor:
         if self.position == 0:
             return None
         else:
-            return self.text[self.position - 1 :]
+            return self.cursorable.content[self.position - 1 :]
     
     @property
     def next_character(self) -> str | None:
-        if (self.position + 1) == len(self.text):
+        if (self.position + 1) == len(self.cursorable.content):
             return None
         else:
-            next_character = self.text[self.position : self.position + 1]
+            next_character = self.cursorable.content[self.position : self.position + 1]
             is_space = next_character == chr(32)
             return None if is_space else next_character
+        
+    def perform(self, action: CursorAction):
+        action.run()
+
+
+class AddCharacter(CursorAction):
+    def __init__(self, cursor: Cursor, character: str):
+        self.cursor = cursor
+        self.character = character
+
+    def run(self):
+        self.cursor.cursorable.append(self.character)
