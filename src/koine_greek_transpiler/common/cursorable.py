0732af1097ba123bcf103eca6690aedbb5047e17
Paul Han <paulh@dtric.com>
2025-03-04 09:57:48 -1000

    Add append method

--- src/koine_greek_transpiler/common/cursorable.py
+++ src/koine_greek_transpiler/common/cursorable.py
@@ -4,6 +4,9 @@ class Cursorable:
     def __init__(self, initial_content: str = ''):
         self.content = initial_content
 
+    def append(self, to_append: str):
+        self.content += to_append
+
     def replace_at(self, index: int, replace_with: str):
         self.content = utils.concat([self.content[:index],
                                      replace_with,
