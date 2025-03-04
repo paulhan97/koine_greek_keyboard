a785fd42857e6ff33c9c9de58bdea54338dbf72f
Paul Han <paulh@dtric.com>
2025-03-04 09:48:05 -1000

    Add cursorable object

---
+++ src/koine_greek_transpiler/common/cursorable.py
@@ -0,0 +1,10 @@
+import koine_greek_transpiler.common.utils as utils
+
+class Cursorable:
+    def __init__(self, initial_content: str = ''):
+        self.content = initial_content
+
+    def replace_at(self, index: int, replace_with: str):
+        self.content = utils.concat([self.content[:index],
+                                     replace_with,
+                                     self.content[index + 1]])
